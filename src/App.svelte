65eeb81555f1515a3d46441091c3f71edc335aea
Reno Zaffina <Razaffina1@gmail.com>
2021-01-28 22:45:21 -0500

    added home view

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
 	import { Router, Route } from "svelte-routing"
+	import Home from './views'
 </script>
 
 <Router basepath='community-locker-self-storage/'>
-	<Route>home</Route>
+	<Route><Home /></Route>
 	<Route path='reservation'>reservation comming soon</Route>
 	<Route path='storage'>storage comming soon</Route>
 </Router>
---
+++ src/views/index.js
@@ -0,0 +1,3 @@
+import Home from './home.svelte'
+
+export default Home
